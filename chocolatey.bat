8e0e4b45d66cfa99dd9a7df5d1ac32a6e6869b77
marcocspc <marcocspc@hotmail.com>
2022-12-13 16:29:10 -0300

    add: script para instalacao de softwares basicos usando o chocolatey

---
+++ chocolatey.bat
@@ -0,0 +1,1 @@
+choco install -y firefox googlechrome libreoffice-still microsoftsecurityessentials python notepadplusplus openjdk vlc audacity audacity-lame k-litecodecpackmega teamviewer gimp inkscape greenshot 7zip
