370c534cb8906a26c0ac66c25252f7866d00db97
marcocspc <marcocspc@hotmail.com>
2022-12-31 18:40:13 -0300

    add: scripts powershell para chocolatey e winget

--- chocolatey.bat
+++
@@ -1,1 +0,0 @@
-choco install -y firefox googlechrome libreoffice-still microsoftsecurityessentials python notepadplusplus openjdk vlc audacity audacity-lame k-litecodecpackmega teamviewer gimp inkscape greenshot 7zip
---
+++ winget.ps1
@@ -0,0 +1,15 @@
+winget install -e --id `
+   Mozilla.Firefox `
+   Google.Chrome `
+   TheDocumentFoundation.LibreOffice `
+   Python.Python.3.11 `
+   Notepad++.Notepad++ `
+   ojdkbuild.openjdk.11.jdk `
+   VideoLAN.VLC `
+   Audacity.Audacity `
+   CodecGuide.K-LiteCodecPack.Mega `
+   TeamViewer.TeamViewer `
+   GIMP.GIMP `
+   Inkscape.Inkscape `
+   Greenshot.Greenshot `
+   7zip.7zip
